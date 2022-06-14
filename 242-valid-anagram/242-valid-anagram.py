26b27efb107be80102ba3f22dcd1eef28f275e66
Prabesh Pokharel <ppokhare@ucsd.edu>
2022-06-14 08:05:03 -0700

    Time: 51 ms (67.98%), Space: 15.3 MB (15.74%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -8,12 +8,12 @@ class Solution(object):
         #idea1: for each char in s find sam char in t and cross that off. Runtime: O(N^2) Space: O(1)
         
         
-        #idea2: create two hashtable and comapre both are true. Runtume: O(n+n+n) Space: O(N+N)
+        #idea2: create two hashtable and comapre both are true. Runtume: O(n+n+n) Space: O(n+n)
+        #return Counter(s) == Counter(t)
         
         #idea 3: instead of two table create one. +1 for s and -1 for t and check if all value is 0. Runtime:O(n+n) Space: (n)
         
         if len(s) != len(t): return False #edge case
-        
         anaDict = defaultdict(int)
         for i in range(len(s)):
             anaDict[s[i]] +=1
