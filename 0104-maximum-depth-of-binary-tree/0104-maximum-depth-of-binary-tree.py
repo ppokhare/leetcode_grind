c29e25150dab0490dc0a39d22064aa8099a923b0
Prabesh Pokharel <ppokhare@ucsd.edu>
2023-11-02 23:17:00 -0700

    Time: 42 ms (79.37%), Space: 17.4 MB (99.78%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -6,16 +6,36 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        
+        #preorder DFS iteratively
         if not root: return 0
-        
-        maxD = 1
         stack = [(root,1)]
+        res = 1
         while stack:
-            node,d = stack.pop()
-            maxD = max(d, maxD)
-            if node.left: stack.append((node.left, d+1))
-            if node.right: stack.append((node.right, d+1))
-            # print(node.val)
-        return maxD
+            node, depth = stack.pop()
+            if node.left:
+                stack.append((node.left,depth+1))
+            if node.right:
+                stack.append((node.right,depth+1))
+            res = max(res, depth)
+        return res
+    
+        
+#         # DFS recursively
+#         if not root: return 0 # if empty
+#         return 1+ max(self.maxDepth(root.left), self.maxDepth(root.right))
+    
+    
+#         #BFS
+#         if not root: return 0    
+#         traverse = deque() # init queue
+#         traverse.append(root)# put root in queue
+#         level = 0
+        
+#         while traverse: # while queue is not empty
+#             for i in range(len(traverse)): #go thur each elt in queue, remove it and put its children
+#                 node = traverse.popleft()
+#                 if node.left: traverse.append(node.left)
+#                 if node.right: traverse.append(node.right)
+#             level +=1
+#         return  level
         
