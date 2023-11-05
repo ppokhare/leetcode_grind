494431bac482f6efd3473cf32e19172819556757
Prabesh Pokharel <ppokhare@ucsd.edu>
2023-11-04 21:29:26 -0700

    Time: 33 ms (87.04%), Space: 16.3 MB (60.51%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -6,25 +6,34 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        
+        #########recursive solution
         if not p and not q: return True
-        if (p and not q) or (not p and q): return False
+        if (p and not q) or (not p and q): return False  
         
-        stack = [(p,q)]
+        if p.val !=q.val: return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
         
-        while stack:
-            p_node, q_node = stack.pop()
+#         ########iterative solution
+#         if not p and not q: return True
+#         if (p and not q) or (not p and q): return False
+        
+#         stack = [(p,q)]
+        
+#         while stack:
+#             p_node, q_node = stack.pop()
             
-            if p_node.val !=   q_node.val: return False
+#             if p_node.val !=   q_node.val: return False
             
-            if p_node.left and q_node.left: stack.append((p_node.left, q_node.left))
-            elif  not p_node.left and  not q_node.left: pass
-            else: return 
+#             if p_node.left and q_node.left: stack.append((p_node.left, q_node.left))
+#             elif  not p_node.left and  not q_node.left: pass
+#             else: return 
             
-            if p_node.right and q_node.right: stack.append((p_node.right, q_node.right))
-            elif  not p_node.right and  not q_node.right: pass
-            else: return False
+#             if p_node.right and q_node.right: stack.append((p_node.right, q_node.right))
+#             elif  not p_node.right and  not q_node.right: pass
+#             else: return False
             
         
-        return True
+#         return True
             
         
